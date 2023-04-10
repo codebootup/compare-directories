ed83b7f5b0160142e3ee8afe23e72efbbb9e953e
Lee Cooper <lee@codebootup.com>
2023-04-10 17:30:53 +0800

    #2 - Pipeline to push to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ object Meta {
     const val desc = "Main use case is for test assertions where you want to compare two directories to identify any differences in tree structure or file content."
     const val license = "Apache-2.0"
     const val githubRepo = "codebootup/compare-directories"
-    const val release = "https://s01.oss.sonatype.org/service/local/"
+    const val release = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
     const val snapshot = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
 }
 
